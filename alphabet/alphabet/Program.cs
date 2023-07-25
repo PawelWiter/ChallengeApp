da41e25f8daf6e03bfbebc3ae247b65da620b48c
pawelwiter <pawel.witer@gmail.com>
2023-07-26 00:13:16 +0200

    Zadania dodatkowe - alfabet

---
+++ alphabet/alphabet/Program.cs
@@ -0,0 +1,20 @@
+﻿//the easiest one
+char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+foreach (char c in alphabet)
+{
+    Console.Write(c);
+}
+Console.WriteLine("");
+Console.WriteLine("--------------------------");
+
+
+//second solution
+for (char c = 'a'; c <= 'z'; c++)
+{
+    Console.Write(c);
+}
+Console.WriteLine("");
+Console.WriteLine("--------------------------");
+
+
