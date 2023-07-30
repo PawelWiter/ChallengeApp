96943d2404181aa619fc997ce1be4cec31b4388b
pawelwiter <pawel.witer@gmail.com>
2023-07-30 21:45:16 +0200

    called ChallengeApp back

---
+++ ChallengeApp.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            //arrange
+
+            int age1 = 10;
+            int age2 = 3;
+
+            //act
+            int result = age1 + age2;   
+
+            //assert
+            Assert.AreEqual(13, result);
+
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//var myAge = 42;
+//liczby calkowite
+using System.Data.Common;
+
+int myAge = 120;
+int newAge = myAge + 5;
+
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+long myVar3 = long.MaxValue;
+
+//liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+//zmienne tekstowe
+string name = "Paweł";
+string surname = "Witer";
+string result = name + surname + myAge;
+//Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray()
+
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
