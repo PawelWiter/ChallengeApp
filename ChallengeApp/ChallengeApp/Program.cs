ecb8b27e372c7ed995e82976574e422133166c51
pawelwiter <pawel.witer@gmail.com>
2023-08-02 09:37:42 +0200

    day 10 #2

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,20 +7,20 @@ Employee employee3 = new Employee("Zenon", "Nowak", 55);
 
 employee1.AddScore(2.5);
 employee1.AddScore("40");
-employee1.AddScore(12);
+employee1.AddScore(12.5445456);
 employee1.AddScore(30);
 employee1.AddScore(40);
 
 employee2.AddScore(1);
-employee2.AddScore(3);
+employee2.AddScore(3.55);
 employee2.AddScore(4);
-employee2.AddScore(5);
+employee2.AddScore(5D);
 employee2.AddScore(6);
 
 employee3.AddScore(5);
-employee3.AddScore(4);
-employee3.AddScore(3);
-employee3.AddScore(2);
+employee3.AddScore("66");
+employee3.AddScore(33.33333);
+employee3.AddScore(0.23333333333333);
 employee3.AddScore(1);
 
 List<Employee> employees = new List<Employee>()
